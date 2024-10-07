d7019441d9423323bc7815ea4b250ead9493a1f7
gelich <budaragina.angelina@gmail.com>
2024-10-07 14:02:17 +0300

    Initial commit

---
+++ app/src/main/java/com/example/homework/MainActivity3.java
@@ -0,0 +1,32 @@
+package com.example.homework;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main3);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.ConstraintLayout), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void onClick(View v){
+        Intent intent = new Intent(this, MainActivity4.class);
+        startActivity(intent);
+    }
+
+}
